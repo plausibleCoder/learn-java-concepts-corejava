6ec9b4016ef971599f798f7173efe14dcfa7907c
plausibleCoder <shivakarthik.wis@gmail.com>
2025-10-01 21:59:07 +0530

    Generics and collections teaching

--- src/main/java/com/learn/java/corejava/learn/collectionframework/generics/GenericsBoundedTypes_3.java
+++ src/main/java/com/learn/java/corejava/learn/collectionframework/generics/GenericsBoundedTypes_3.java
@@ -16,8 +16,18 @@ class BoundedTypeExample {
     public static void main(String[] args) {
         Integer[] integers = {1, 2, 3, 4, 5};
         Double[] doubles = {1.5, 2.5, 3.5};
-
+        String[] string = {"one","Two","Three"};
         System.out.println("Average of integers: " + GenericsBoundedTypes_3.getAverage(integers));
         System.out.println("Average of doubles: " + GenericsBoundedTypes_3.getAverage(doubles));
+       // GenericsBoundedTypes_3.getAverage(string));
     }
+    
+   /*
+    Select 
+    Groupby
+    Having 
+    joins
+    Where */
+    
+    
 }
